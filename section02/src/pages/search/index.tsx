0be82b44d7e397b9257c8246081bb09ff8fd5070
jjeongsu <jeongsu.dev@gmail.com>
2024-08-26 12:07:49 +0900

    2.12 SSR

--- section02/src/pages/search/index.tsx
+++ section02/src/pages/search/index.tsx
@@ -1,9 +1,24 @@
 import SearchableLayout from '@/components/searchable-layout'
-import { useRouter } from 'next/router'
 import { ReactNode } from 'react'
-import books from '@/mock/books.json'
 import BookItem from '@/components/book-item'
-export default function Page() {
+import { GetServerSidePropsContext, InferGetServerSidePropsType } from 'next'
+import fetchBooks from '@/lib/fetch-books'
+
+export const getServerSideProps = async (
+  context: GetServerSidePropsContext
+) => {
+  //context: 브라우저 로 부터 받은 요청에 대한 모든 정보 보유
+  const q = context.query.q
+  const books = await fetchBooks(q as string)
+
+  return {
+    props: { books },
+  }
+}
+
+export default function Page({
+  books,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
   return (
     <div>
       {books.map(book => (
