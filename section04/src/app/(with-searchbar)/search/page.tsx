bb8e32224737e6f973fd88b57bd18e731ee45d6e
jjeongsu <jeongsu.dev@gmail.com>
2024-09-05 21:35:25 +0900

    4.1 data fetching

--- section04/src/app/(with-searchbar)/search/page.tsx
+++ section04/src/app/(with-searchbar)/search/page.tsx
@@ -1,13 +1,20 @@
-import books from '../../../mock/book.json'
 import BookItem from '@/components/book-item'
-
-export default function Page({
+import { BookData } from '@/types'
+export default async function Page({
   searchParams,
 }: {
   searchParams: {
     q?: string
   }
 }) {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_SEVER_URL}/book/search?q=${searchParams.q}`
+  )
+  if (!response.ok) {
+    return <div>오류가 발생했습니다...</div>
+  }
+  const books: BookData[] = await response.json()
+
   return (
     <div>
       {books.map((book: any) => (
