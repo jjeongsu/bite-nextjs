feaa25fc3b3a4a538c3d76e32428277b2356d43c
jjeongsu <jeongsu.dev@gmail.com>
2024-09-13 17:08:58 +0900

    skeleton UI

--- section06/src/app/(with-searchbar)/page.tsx
+++ section06/src/app/(with-searchbar)/page.tsx
@@ -3,6 +3,7 @@ import style from './page.module.css'
 import { BookData } from '@/types'
 import { delay } from '@/util/delay'
 import { Suspense } from 'react'
+import BookListSkeleton from '@/components/skeleton/book-list-skeleton'
 
 async function Allbooks() {
   //fetch data
@@ -52,13 +53,14 @@ export default function Home() {
     <div className={style.container}>
       <section>
         <h3>지금 추천하는 도서</h3>
-        <Suspense fallback={<div>도서를 불러오는 중</div>}>
+
+        <Suspense fallback={<BookListSkeleton count={3} />}>
           <Recobooks />
         </Suspense>
       </section>
       <section>
         <h3>등록된 모든 도서</h3>
-        <Suspense fallback={<div>도서를 불러오는 중</div>}>
+        <Suspense fallback={<BookListSkeleton count={1} />}>
           <Allbooks />
         </Suspense>
       </section>
